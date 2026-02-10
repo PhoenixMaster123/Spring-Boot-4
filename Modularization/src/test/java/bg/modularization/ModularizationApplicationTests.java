d4416a6ba3448c939e83ee4856c084ab4853f08a
Kristian Popov <kristianpopov03@yahoo.com>
2026-02-10 16:46:09 +0100

    Spring Boot 4 (Modularization)

---
+++ Modularization/src/test/java/bg/modularization/ModularizationApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.modularization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
